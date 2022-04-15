3b261f659db7d7d2e37642ac3552de8a7c95dec0
Ori David <orid2004@gmail.com>
2022-04-16 00:52:50 +0300

    switched to postimg hosting as imgbb is down

--- selfdrive/model/speedlimit.py
+++ selfdrive/model/speedlimit.py
@@ -19,10 +19,9 @@ class Model(Base):
             label_map_path=os.path.join(self.path, LABELMAP_NAME),
             max_detections=MAX_DETECTIONS,
             warmup_set=[
-                "https://i.ibb.co/tZ4VQPS/Limit-4bff52ca-7a12-11ec-99d4-a44cc849d7a3.jpg",
-                "https://i.ibb.co/y6Q7m1x/Limit-4c6a323a-7a12-11ec-83a9-a44cc849d7a3.jpg",
-                "https://i.ibb.co/znSCPW1/Limit-4c5ee9ca-7a12-11ec-8e7e-a44cc849d7a3.jpg",
-                "https://i.ibb.co/X3s4PsV/Limit-4c1eba6c-7a12-11ec-a633-a44cc849d7a3.jpg"
+                "https://i.postimg.cc/BZzHPrL5/Limit-4bff52ca-7a12-11ec-99d4-a44cc849d7a3.jpg",
+                "https://i.postimg.cc/xTbHv6Mw/Limit-4c0bf458-7a12-11ec-b25b-a44cc849d7a3.jpg",
+                "https://i.postimg.cc/RZ5tXwTN/Limit-4c1c25ac-7a12-11ec-8acd-a44cc849d7a3.jpg"
             ]
 
         )
